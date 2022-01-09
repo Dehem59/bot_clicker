7ee7fc8ce2a03565d1446df04d5be93d5cdb5f88
Victor Dehem <victor.dehem.etu@univ-lille.fr>
2022-01-09 01:51:50 +0100

    update settings heroku

--- bot_clicker/wsgi.py
+++ bot_clicker/wsgi.py
@@ -11,6 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bot_clicker.settings')
+if os.getenv('DJANGO_SETTINGS_MODULE'):
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', os.getenv('DJANGO_SETTINGS_MODULE'))
+else:
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', "bot_clicker.dev")
 
 application = get_wsgi_application()
