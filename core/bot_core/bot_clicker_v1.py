82448572c97c6f764c4a77629445f75f95582029
Zack099 <Zakaria.hairane@gmail.com>
2022-01-09 20:02:47 +0100

    core(02):bot_FirstVersion

--- core/bot_core/bot_clicker_v1.py
+++ core/bot_core/bot_clicker_v1.py
@@ -1,6 +1,12 @@
 import os
+import random
 import time
 
+from bo.models.siteweb import SiteWeb
+from bo.models.requete import Requete
+from bo.models.aclicker import Aclicker
+
+from selenium.webdriver import DesiredCapabilities
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from seleniumwire import webdriver
@@ -9,11 +15,11 @@ from selenium.webdriver.common.action_chains import ActionChains
 
 # GOOGLE_CHROME_BIN = os.getenv("GOOGLE_CHROME_BIN")
 # CHROMEDRIVER_PATH = os.getenv("CHROMEDRIVER_PATH")
-
+RANDOM_VARIABLE = {"scroll": [37, 58, 76, 117, 221], "delay": [1.05, 1.6, 1.2, 2.11, 3.07, 3.34]}
 
 class BotClickerV1:
 
-    def __init__(self, proxy, query, mode=True, domain=None, headless=True):
+    def __init__(self, proxy, query, domain=None, online=True):
         """
 
         :param proxy: proxy dict
@@ -21,11 +27,13 @@ class BotClickerV1:
         """
         self.proxy = proxy
         self.query = query
-        self.driver = self.init_webdriver(headless)
-        self.mode = mode
         self.domain = domain
+        self.online = online
+        self.driver = self.init_webdriver()
 
-    def init_webdriver(self, headless):
+
+    def init_webdriver(self):
+
         options = {
             'proxy': {
                 'http': f'http://{self.proxy["user"]}:{self.proxy["pass"]}@{self.proxy["host"]}:{self.proxy["port"]}',
@@ -34,74 +42,75 @@ class BotClickerV1:
             }
         }
         chrome_options = webdriver.ChromeOptions()
-        chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument('--user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X)'
                                     ' AppleWebKit/602.1.50 (KHTML, like Gecko) CriOS/56.0.2924.75 Mobile/14E5239e '
                                     'Safari/602.1')
+        capabilities = DesiredCapabilities.CHROME.copy()
+        capabilities["device"] = "iPhone 12 Pro"
+        capabilities["real_mobile"] = True
 
-        driver = webdriver.Chrome(executable_path=os.getenv("CHROMEDRIVER_PATH"), chrome_options=chrome_options,
+        if self.online:
+            chrome_options.add_argument("--headless")
+            chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+            driver = webdriver.Chrome(executable_path=os.getenv("CHROMEDRIVER_PATH"), chrome_options=chrome_options,
                                   seleniumwire_options=options)
+        else:
+            driver = webdriver.Chrome(ChromeDriverManager().install(), desired_capabilities=capabilities,
+                                  chrome_options=chrome_options, seleniumwire_options=options)
+            driver.set_window_position(50, 0)
+            driver.set_window_size(390, 844)
         return driver
+
 
     def find_res_natural(self):
         result = self.driver.find_element(By.ID, "rso")
         for res in result.find_elements(By.CSS_SELECTOR, ".uUPGi"):
             actions = ActionChains(self.driver)
             actions.move_to_element(res).perform()
-            presentation = res.find_element(By.CSS_SELECTOR, "[role=presentation]")
+            presentation = res.find_element(By.CSS_SELECTOR, "a[role=presentation]")
             if self.domain in presentation.get_attribute("href"):
                 presentation.click()
+                time.sleep(0.5)
                 return True
             time.sleep(0.2)
         return False
-
-    def find_res_ads(self):
-        annonces = self.driver.find_elements(By.CSS_SELECTOR, "[aria-label=Annonces] .uEierd")
-        result = []
-        if len(annonces) > 0:
-            for annonce in annonces:
-                actions = ActionChains(self.driver)
-                actions.move_to_element(annonce).perform()
-                time.sleep(0.5)
-                annonce.find_element(By.CSS_SELECTOR, "[role=heading]").click()
-                url, title = self.website_action()
-                result.append({"url": url, "title": title})
-                time.sleep(1.2)
-                self.driver.back()
-                time.sleep(1)
-        return result
 
     def website_action(self):
         """
 
         :return:
         """
-        delay_sleep = 1 if self.mode else 2
-        print("On website")
+        last_offset = 0
+        for nb_scroll in range(21):
+            offset = random.choice(RANDOM_VARIABLE["scroll"])
+            delay = random.choice(RANDOM_VARIABLE["delay"])
+            self.driver.execute_script(f"window.scrollTo({last_offset}, {last_offset + offset})")
+            time.sleep(delay)
+            last_offset += offset
 
-        try:
-            title = self.driver.find_element(By.TAG_NAME, "h1").text
-        except:
-            title = self.driver.find_element(By.TAG_NAME, "p").text
-        # for offset in range(0, 910, 70):
-        #     self.driver.execute_script(f"window.scrollTo({offset}, {offset + 70})")
-        time.sleep(delay_sleep)
-        return str(self.driver.current_url), title
+    def database_maj(self,found,delay,pos):
+        r=Requete.objects.create(libelle= self.query)
+        s=SiteWeb.objects.get_or_create(url=self.driver.current_url)
+        s.requetes.add(r, through_defaults={'resultat': found,'proxy': self.proxy["host"], 'timescrolling':delay, 'positition_page':pos})
+
 
     def execute(self):
-        self.driver.get(f'https://www.google.com/search?q={self.query}')
-        time.sleep(1)
-        result = []
-        if self.mode:
-            result = self.find_res_ads()
-        else:
-            found = self.find_res_natural()
-            self.website_action()
-            if not found:
+        i = 0
+        while i <= 50:
+            self.driver.get(f'https://www.google.com/search?q={self.query}&start={i}')
+            i += 10
+            time.sleep(1)
+            if self.find_res_natural():
+                time_before = time.time()
+                self.website_action()
+                time_after = time.time()
+                delay = time_before-time_after
+                self.database_maj("Found",delay, i)
+            else:
+                self.database_maj("NotFound",0, i)
                 print("Website not in 1st page")
                 self.driver.close()
-                exit()
-        return result
