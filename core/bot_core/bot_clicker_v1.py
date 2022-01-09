41e816ae010a9fbe675b7a0dd258ad2cd80ea087
Victor Dehem <victor.dehem.etu@univ-lille.fr>
2022-01-09 02:33:11 +0100

    update settings heroku

--- core/bot_core/bot_clicker_v1.py
+++ core/bot_core/bot_clicker_v1.py
@@ -41,7 +41,8 @@ class BotClickerV1:
         chrome_options.binary_location = GOOGLE_CHROME_BIN
         chrome_options.add_argument('--disable-gpu')
         chrome_options.add_argument('--no-sandbox')
-
+        chrome_options.add_argument("--disable-extensions")
+        chrome_options.add_argument("--disable-dev-shm-usage")
         if headless:
             chrome_options.add_argument('--headless')
 
