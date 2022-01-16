334cab1e77afcce7f3a7480f29690722f0681ff1
Victor Dehem <victor.dehem.etu@univ-lille.fr>
2022-01-16 21:23:37 +0100

    launch auto process

--- gui/views/exec_auto.py
+++ gui/views/exec_auto.py
@@ -1,9 +1,11 @@
+import random
+
 from django.http import JsonResponse
 from django.views import View
 from django.shortcuts import render
 
 from bo.models import UserAgent, Keyword, Proxy
-
+from core.bot_core.bot_clicker_v1 import BotClickerV1
 
 
 class ExecutionAutoView(View):
@@ -19,7 +21,40 @@ class ExecutionAutoView(View):
         )
 
     def post(self, request, *args, **kwargs):
-        pass
+        nb_proxy = int(request.POST["proxy"])
+        queries = request.POST.getlist("queries[]")
+        domaine = request.POST["domaine"]
+        online = False
+        user_agent = UserAgent.objects.get(nom=request.POST["user_agent"])
+        keywords = Keyword.objects.filter(pk__in=queries)
+        # ref = launch_bot.delay(params)
+        cpt = 0
+        proxies = list(Proxy.objects.all())
+        seen = set()
+        results = {"detail": False, "results": []}
+        while cpt < nb_proxy:
+            cpt += 1
+            current_proxy = random.choice(proxies)
+            while current_proxy.host in seen:
+                current_proxy = random.choice(proxies)
+            seen.add(current_proxy.host)
+            for keyword in keywords:
+                try:
+                    dict_proxy = {
+                        "host": current_proxy.host, "port": current_proxy.port, "user": current_proxy.user,
+                        "pass": current_proxy.password
+                    }
+                    tmp_bot = BotClickerV1(proxy=dict_proxy, query=keyword.google_correspondance,
+                                           domain=domaine, user_agent=user_agent.definition, online=False)
+                    res = tmp_bot.execute()
+                    results["results"].append(res)
+                except Exception as exc:
+                    if "errors" not in results:
+                        results["errors"] = [str(exc)]
+                    else:
+                        results["errors"].append(str(exc))
+
+        return JsonResponse(results, status=200)
 
 
 
